7844bf49a32c43492bf529d3209c30cd0191be9d
Mezbah Uddin <163080472+Mezbah143@users.noreply.github.com>
2026-04-16 23:59:58 +0900

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-cp -R frontend/* "/Users/mezbahuddin/Desktop/apache-tomcat-10.1.54/webapps/StudentBridge/"
+PROJECT="/Users/mezbahuddin/Capstone Design/Student Brigde /StudentBridge"
+TOMCAT="/Users/mezbahuddin/Desktop/apache-tomcat-10.1.54/webapps/StudentBridge"
 
-javac -cp "./docs/mysql-connector-j-9.3.0.jar:./docs/servlet-api.jar" -d bin Backend/*.java
+mkdir -p "$TOMCAT/WEB-INF/classes"
+mkdir -p "$TOMCAT/WEB-INF/lib"
 
-cp bin/*.class "/Users/mezbahuddin/Desktop/apache-tomcat-10.1.54/webapps/StudentBridge/WEB-INF/classes/"
+rsync -av --exclude ".git" --exclude ".vscode" "$PROJECT/" "$TOMCAT/"
+
+javac -cp "$PROJECT/docs/mysql-connector-j-9.3.0.jar:$PROJECT/docs/servlet-api.jar" -d "$PROJECT/bin" "$PROJECT"/Backend/*.java
+
+cp "$PROJECT"/bin/*.class "$TOMCAT/WEB-INF/classes/"
+cp "$PROJECT/docs/mysql-connector-j-9.3.0.jar" "$TOMCAT/WEB-INF/lib/"
 
 echo "Deployment Complete!"
-
-
-
