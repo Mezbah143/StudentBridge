5373259dd3a70c1d37a6219c5306b98aa1f848f4
Mezbah Uddin <163080472+Mezbah143@users.noreply.github.com>
2026-04-17 20:13:03 +0900

    Folder Structure

--- server/models/job.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const JobSchema = new mongoose.Schema({
-  title: String,
-  city: String,
-  createdBy: String
-});
-
-module.exports = mongoose.model("Job", JobSchema);
